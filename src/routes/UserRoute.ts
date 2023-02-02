25d5831eacac62eaa9fb048d1ec656ab51a9ed75
Ed1n <Jdei_nin230@hotmail.com>
2023-02-02 18:16:03 +0700

    add user route

---
+++ src/routes/UserRoute.ts
@@ -0,0 +1,12 @@
+import express from "express";
+import {
+    getUsers,
+    createUser,
+} from "../controllers/Users";
+
+const router = express.Router();
+
+router.get('/users', getUsers);
+router.post('/users', createUser);
+
+export default router;
