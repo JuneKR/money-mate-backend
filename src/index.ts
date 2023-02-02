a60eadbf8b090e8563ddce466bbde29615e5f58f
Ed1n <Jdei_nin230@hotmail.com>
2023-02-02 14:19:14 +0700

    connect to the database

--- src/index.ts
+++ src/index.ts
@@ -1,13 +1,46 @@
 import { config } from 'dotenv';
 import express, { Application, NextFunction, Express, Request, Response } from 'express';
+import cors from 'cors';
+import session from "express-session";
+import SequelizeStore from 'connect-session-sequelize';
+import db from './config/Database';
 
 config();
 
 const app: Application = express();
 
+const sessionStore = SequelizeStore(session.Store);
+
+const store = new sessionStore({
+    db: db
+})
+
+const SECRET = process.env.SESS_SECRET
+
+if(!SECRET) {
+    throw new Error('Messing Session Secret!')
+}
+
+app.use(session({
+    secret: SECRET,
+    resave: false,
+    saveUninitialized: true,
+    store: store,
+    cookie: {
+        secure: 'auto'
+    }
+}))
+
+app.use(cors({
+    credentials: true,
+    origin: 'http://localhost:3000'
+}))
+
 app.get('/', (req: Request, res: Response, next: NextFunction) => {
     res.send('Hello, World!')
 })
+
+app.use(express.json());
 
 const PORT = process.env.PORT || 8080;
 
