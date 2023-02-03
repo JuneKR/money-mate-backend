da8e12ccc75df25046ea71e207dcdadf63935e77
Ed1n <Jdei_nin230@hotmail.com>
2023-02-03 15:50:39 +0700

    add user routes

--- src/routes/UserRoute.ts
+++ src/routes/UserRoute.ts
@@ -2,11 +2,17 @@ import express from "express";
 import {
     getUsers,
     createUser,
+    getUserById,
+    updateUser,
+    deleteUser
 } from "../controllers/Users";
 
 const router = express.Router();
 
 router.get('/users', getUsers);
+router.get('/users/:id', getUserById);
 router.post('/users', createUser);
+router.patch('/users/:id', updateUser)
+router.delete('/users/:id', deleteUser)
 
 export default router;
